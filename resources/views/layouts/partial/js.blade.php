e77c6376269dc53ffe8e79c350357d0c964d2642
Steve Nyanumba <reach@stevenyanumba.com>
2022-02-28 13:27:55 +0300

    First Commit to test the final product

--- resources/views/layouts/partial/js.blade.php
+++ resources/views/layouts/partial/js.blade.php
@@ -1,11 +1,11 @@
-<script src="{{ asset('client/js/vendor/jquery.js') }}"></script>
-<script src="{{ asset('client/js/vendor/modernizer.min.js') }}"></script>
-<script src="{{ asset('client/js/vendor/feather.min.js') }}"></script>
-<script src="{{ asset('client/js/vendor/slick.min.js') }}"></script>
-<script src="{{ asset('client/js/vendor/bootstrap.js') }}"></script>
-<script src="{{ asset('client/js/vendor/text-type.js') }}"></script>
-<script src="{{ asset('client/js/vendor/wow.js') }}"></script>
-<script src="{{ asset('client/js/vendor/aos.js') }}"></script>
-<script src="{{ asset('client/js/vendor/particles.js') }}"></script>
+<script src="/client/js/vendor/jquery.js"></script>
+<script src="/client/js/vendor/modernizer.min.js"></script>
+<script src="/client/js/vendor/feather.min.js"></script>
+<script src="/client/js/vendor/slick.min.js"></script>
+<script src="/client/js/vendor/bootstrap.js"></script>
+<script src="/client/js/vendor/text-type.js"></script>
+<script src="/client/js/vendor/wow.js"></script>
+<script src="/client/js/vendor/aos.js"></script>
+<script src="/client/js/vendor/particles.js"></script>
 <!-- main JS -->
-<script src="{{ asset('client/js/main.js') }}"></script>
+<script src="/client/js/main.js"></script>
