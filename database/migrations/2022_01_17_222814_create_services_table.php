2c79f1082b25d2fb4880e37a6f30bdea6eab61e0
Steve Nyanumba <reach@stevenyanumba.com>
2022-03-07 16:20:20 +0300

    The rectification

--- database/migrations/2022_01_17_222814_create_services_table.php
+++ database/migrations/2022_01_17_222814_create_services_table.php
@@ -29,14 +29,17 @@ class CreateServicesTable extends Migration
                 'description' => 'Editorial images offer photographers a way to explore their creative freedom while telling a story.'
             ],
             [
+                'service_category_id' => 1,
                 'title' => 'Advertising Photography',
                 'description' => "Good advertising photography persuades you to purchase something that you don't really need. It works by putting products, goods or services in front of an audience, and then convincing them to be interested."
             ],
             [
+                'service_category_id' => 2,
                 'title' => 'Portrait Photography',
                 'description' => "Good portrait photographers are able to capture the personality and emotion of people around them, along with earning money via wedding photography, senior portraits, family photography sessions, and so on."
             ],
             [
+                'service_category_id' => 2,
                 'title' => 'Food & Drink Photography',
                 'description' => 'It builds a brand and tells a story. As a restaurant owner, you have the chance to control how the world perceives your restaurant. You are in control of what you show and what image you put out to the world. Professional food photos can help you build a brand that stands out and shapes your restaurant identity.'
             ],
