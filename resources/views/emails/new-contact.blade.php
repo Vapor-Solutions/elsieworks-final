becb901ddf09920a8d3060785db50a4ab7ab5bc6
Steve Nyanumba <reach@stevenyanumba.com>
2022-04-22 19:53:57 +0300

    The email for the new contact has been formated

--- resources/views/emails/new-contact.blade.php
+++ resources/views/emails/new-contact.blade.php
@@ -6,39 +6,197 @@
 
 </head>
 
-<body>
-    <div class="container">
-        <div class="text-center">
-            <img src="/ew-secondary.png" width="150" alt="">
-        </div>
-        <div class="my-5">
-            <h1 class="text-center">
-                New Contact Needs Assistance
-            </h1>
-            <p>
-                There is a new contact that needs assistance. Go check your admin panel to find out more.
-            </p>
+<body style="background-color: #FFFFFF; margin: 0; padding: 0; -webkit-text-size-adjust: none; text-size-adjust: none;">
+    <table border="0" cellpadding="0" cellspacing="0" class="nl-container" role="presentation"
+        style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; background-color: #FFFFFF;" width="100%">
+        <tbody>
+            <tr>
+                <td>
+                    <table align="center" border="0" cellpadding="0" cellspacing="0" class="row row-1"
+                        role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt;" width="100%">
+                        <tbody>
+                            <tr>
+                                <td>
+                                    <table align="center" border="0" cellpadding="0" cellspacing="0"
+                                        class="row-content stack" role="presentation"
+                                        style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; color: #000000; width: 500px;"
+                                        width="500">
+                                        <tbody>
+                                            <tr>
+                                                <td class="column column-1"
+                                                    style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; font-weight: 400; text-align: left; vertical-align: top; padding-top: 5px; padding-bottom: 5px; border-top: 0px; border-right: 0px; border-bottom: 0px; border-left: 0px;"
+                                                    width="100%">
+                                                    <table border="0" cellpadding="0" cellspacing="0"
+                                                        class="heading_block" role="presentation"
+                                                        style="mso-table-lspace: 0pt; mso-table-rspace: 0pt;"
+                                                        width="100%">
+                                                        <tbody>
+                                                            <tr>
+                                                                <td style="width:100%;text-align:center;">
+                                                                    <h1
+                                                                        style="margin: 0; color: #555555; font-size: 26px; font-family: 'Roboto', Tahoma, Verdana, Segoe, sans-serif; line-height: 120%; text-align: center; direction: ltr; font-weight: 700; letter-spacing: normal; margin-top: 0; margin-bottom: 0;">
+                                                                        <span class="tinyMce-placeholder">There's a new
+                                                                            contact for you</span></h1>
+                                                                </td>
+                                                            </tr>
+                                                        </tbody>
+                                                    </table>
+                                                    <table border="0" cellpadding="10" cellspacing="0"
+                                                        class="paragraph_block" role="presentation"
+                                                        style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; word-break: break-word;"
+                                                        width="100%">
+                                                        <tbody>
+                                                            <tr>
+                                                                <td>
+                                                                    <div
+                                                                        style="color:#000000;font-size:16px;font-family:'Roboto', Tahoma, Verdana, Segoe, sans-serif;font-weight:400;line-height:120%;text-align:left;direction:ltr;letter-spacing:0px;">
+                                                                        <p style="margin: 0;">There is a new contact
+                                                                            that needs assistance. Go check your admin
+                                                                            panel to find out more.</p>
+                                                                    </div>
+                                                                </td>
+                                                            </tr>
+                                                        </tbody>
+                                                    </table>
+                                                    <table border="0" cellpadding="10" cellspacing="0"
+                                                        class="list_block" role="presentation"
+                                                        style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; word-break: break-word;"
+                                                        width="100%">
+                                                        <tbody>
+                                                            <tr>
+                                                                <td>
+                                                                    <ul
+                                                                        style="margin: 0; padding: 0; margin-left: 20px; color: #000000; font-size: 14px; font-family: Arial, Helvetica Neue, Helvetica, sans-serif; font-weight: 400; line-height: 120%; text-align: left; direction: ltr; letter-spacing: 0px;">
+                                                                        <li style="list-style-type: none;">
+                                                                            <ul
+                                                                                style="margin: 0; padding: 0; margin-top: 0px;">
+                                                                                <li
+                                                                                    style="margin-bottom: 0px; margin-left: 40px;">
+                                                                                    <strong>Name:&nbsp;</strong>{{ $details->name }}</li>
+                                                                                <li
+                                                                                    style="margin-bottom: 0px; margin-left: 40px;">
+                                                                                    <strong>Email:&nbsp;</strong>{{ $details->email }}
+                                                                                </li>
+                                                                                <li style="margin-left: 40px;">
+                                                                                    <strong>Phone Number:</strong> {{$details->phone_number}}</li>
+                                                                            </ul>
+                                                                        </li>
+                                                                    </ul>
+                                                                </td>
+                                                            </tr>
+                                                        </tbody>
+                                                    </table>
+                                                    <table border="0" cellpadding="10" cellspacing="0"
+                                                        class="divider_block" role="presentation"
+                                                        style="mso-table-lspace: 0pt; mso-table-rspace: 0pt;"
+                                                        width="100%">
+                                                        <tbody>
+                                                            <tr>
+                                                                <td>
+                                                                    <div align="center">
+                                                                        <table border="0" cellpadding="0"
+                                                                            cellspacing="0" role="presentation"
+                                                                            style="mso-table-lspace: 0pt; mso-table-rspace: 0pt;"
+                                                                            width="100%">
+                                                                            <tbody>
+                                                                                <tr>
+                                                                                    <td class="divider_inner"
+                                                                                        style="font-size: 1px; line-height: 1px; border-top: 1px solid #BBBBBB;">
+                                                                                        <span> </span></td>
+                                                                                </tr>
+                                                                            </tbody>
+                                                                        </table>
+                                                                    </div>
+                                                                </td>
+                                                            </tr>
+                                                        </tbody>
+                                                    </table>
+                                                    <table border="0" cellpadding="0" cellspacing="0"
+                                                        class="heading_block" role="presentation"
+                                                        style="mso-table-lspace: 0pt; mso-table-rspace: 0pt;"
+                                                        width="100%">
+                                                        <tbody>
+                                                            <tr>
+                                                                <td style="width:100%;text-align:center;">
+                                                                    <h1
+                                                                        style="margin: 0; color: #555555; font-size: 23px; font-family: Arial, Helvetica Neue, Helvetica, sans-serif; line-height: 120%; text-align: center; direction: ltr; font-weight: 700; letter-spacing: normal; margin-top: 0; margin-bottom: 0;">
+                                                                        <span
+                                                                            class="tinyMce-placeholder">Subject:&nbsp;<u>{{$details->subject}}</u></span></h1>
+                                                                </td>
+                                                            </tr>
+                                                        </tbody>
+                                                    </table>
+                                                    <table border="0" cellpadding="10" cellspacing="0"
+                                                        class="paragraph_block" role="presentation"
+                                                        style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; word-break: break-word;"
+                                                        width="100%">
+                                                        <tbody>
+                                                            <tr>
+                                                                <td>
+                                                                    <div
+                                                                        style="color:#000000;font-size:14px;font-family:Arial, Helvetica Neue, Helvetica, sans-serif;font-weight:400;line-height:120%;text-align:left;direction:ltr;letter-spacing:0px;">
+                                                                        <p style="margin: 0;">{{ $details->message }}</p>
+                                                                    </div>
+                                                                </td>
+                                                            </tr>
+                                                        </tbody>
+                                                    </table>
+                                                    <table border="0" cellpadding="10" cellspacing="0"
+                                                        class="divider_block" role="presentation"
+                                                        style="mso-table-lspace: 0pt; mso-table-rspace: 0pt;"
+                                                        width="100%">
+                                                        <tbody>
+                                                            <tr>
+                                                                <td>
+                                                                    <div align="center">
+                                                                        <table border="0" cellpadding="0"
+                                                                            cellspacing="0" role="presentation"
+                                                                            style="mso-table-lspace: 0pt; mso-table-rspace: 0pt;"
+                                                                            width="100%">
+                                                                            <tbody>
+                                                                                <tr>
+                                                                                    <td class="divider_inner"
+                                                                                        style="font-size: 1px; line-height: 1px; border-top: 3px solid #BE6436;">
+                                                                                        <span> </span></td>
+                                                                                </tr>
+                                                                            </tbody>
+                                                                        </table>
+                                                                    </div>
+                                                                </td>
+                                                            </tr>
+                                                        </tbody>
+                                                    </table>
+                                                    <table border="0" cellpadding="0" cellspacing="0"
+                                                        class="image_block" role="presentation"
+                                                        style="mso-table-lspace: 0pt; mso-table-rspace: 0pt;"
+                                                        width="100%">
+                                                        <tbody>
+                                                            <tr>
+                                                                <td
+                                                                    style="width:100%;padding-right:0px;padding-left:0px;">
+                                                                    <div align="center" style="line-height:10px"><img
+                                                                            src="/ew-secondary.png"
+                                                                            style="display: block; height: auto; border: 0; width: 300px; max-width: 100%;"
+                                                                            width="300"></div>
+                                                                </td>
+                                                            </tr>
+                                                        </tbody>
+                                                    </table>
+                                                </td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </td>
+            </tr>
+        </tbody>
+    </table><!-- End -->
 
-            <p>
-                Name: <strong>{{ $details->name }}</strong>
-            </p>
-            <p>
-                Email: <strong>{{ $details->email }}</strong>
-            </p>
-            <p>
-                Phone Number: <strong>{{ $details->subject }}</strong>
-            </p>
-            <p>
-                Subject: <strong>{{ $details->subject }}</strong>
-            </p>
-            <p>
-                Message: <strong>{{ $details->message }}</strong>
-            </p>
-
-            <p>Thank you!</p>
-            <small>From <strong>system@elsieworks.com</strong></small>
-        </div>
-    </div>
 </body>
-
 </html>
+
+
+
