653d3b021d71e24012962380ac955155a8a4fbae
Steve Nyanumba <reach@stevenyanumba.com>
2022-05-03 02:27:15 +0300

    This is the finishing point

--- resources/views/layouts/partial/admin/sidebar.blade.php
+++ resources/views/layouts/partial/admin/sidebar.blade.php
@@ -2,7 +2,7 @@
     <div class="sidebar-user text-center">
         <a class="setting-primary" href="{{ route('profile.show') }}"><i data-feather="settings"></i></a>
         <img class="img-90 rounded-circle"
-            src="{{ auth()->user()->profile_photo_path ?? auth()->user()->profile_photo_url }}" alt="" />
+            src="{{ auth()->user()->profile_photo_url }}" alt="" />
         <a href="javascript:void(0)">
             <h6 class="mt-3 f-14 f-w-600">{{ auth()->user()->name }}</h6>
         </a>
